ca2e1e2c44b7f5236ca9455b2184dd0655072a95
Kris <kristof.solak@gmail.com>
2021-10-14 16:19:49 +0200

    Init added

--- src/com/company/libraries/Main.java
+++ src/com/company/libraries/Main.java
@@ -2,20 +2,36 @@ package com.company.libraries;
 
 import com.company.libraries.creator.Author;
 import com.company.libraries.creator.aCreator;
+import com.company.libraries.enums.BookType;
 import com.company.libraries.item.Book;
+import com.company.libraries.item.EBook;
 import com.company.libraries.item.aBook;
+import com.company.libraries.library.Library;
 
 public class Main {
 
     public static void main(String[] args) {
 
-        init();
-    }
-    private static void init(){
         // init Authors
-       // aCreator JoshuaB = new Author("Joshua Block");
+        aCreator JoshuaB = new Author("Joshua Block");
+
         // init Book
-       // aBook book = new Book(134685997 ,"Effective Java", (Author) JoshuaB);
+        aBook EffectiveJava = new Book(134685997 ,"Effective Java", (Author) JoshuaB, BookType.SCIENCE);
+        EBook EffectiveJavaEBook = new EBook(134685997 ,"Effective Java", (Author) JoshuaB, BookType.SCIENCE,"url");
+
+        // init libraries
+        Library EdinburghLibrary = new Library("Edinburgh","address");
+        Library GlasgowLibrary = new Library("Glasgow","address");
+
+        // Add books to libraries
+        EdinburghLibrary.addBook(EffectiveJava);
+        EdinburghLibrary.addEBook(EffectiveJavaEBook);
+
+        EdinburghLibrary.rentBook(EffectiveJava);
+        EdinburghLibrary.rentEBook(EffectiveJavaEBook);
+
     }
+
+
 }
 
